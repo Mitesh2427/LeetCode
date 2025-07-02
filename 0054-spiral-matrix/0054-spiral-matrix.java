bd13de8a71dcc9307f5ac18e715b5ab6af11a266
Mitesh2427 <69318991+Mitesh2427@users.noreply.github.com>
2025-07-02 15:41:47 +0530

    Time: 0 ms (100%), Space: 41.5 MB (62.96%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new ArrayList<>();
 
-        if (matrix == null || matrix.length == 0) return list;
+        //if (matrix == null || matrix.length == 0) return list;
 
         int top = 0;
         int bottom = matrix.length - 1;
@@ -10,27 +10,24 @@ class Solution {
         int right = matrix[0].length - 1;
 
         while (top <= bottom && left <= right) {
-            // Traverse top row left to right
+
             for (int j = left; j <= right; j++) {
                 list.add(matrix[top][j]);
             }
             top++;
 
-            // Traverse right column top to bottom
             for (int i = top; i <= bottom; i++) {
                 list.add(matrix[i][right]);
             }
             right--;
 
-            // Traverse bottom row right to left
             if (top <= bottom) {
                 for (int j = right; j >= left; j--) {
                     list.add(matrix[bottom][j]);
                 }
                 bottom--;
             }
-
-            // Traverse left column bottom to top
+            
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     list.add(matrix[i][left]);
